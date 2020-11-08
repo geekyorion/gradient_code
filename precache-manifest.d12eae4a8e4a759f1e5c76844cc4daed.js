f7899cd603318b1c03f2f7c591c1ea6bc8a08573
Shashank Sharma <shashank.ns67@gmail.com>
2020-11-08 20:19:35 +0530

    Updates

---
+++ precache-manifest.d12eae4a8e4a759f1e5c76844cc4daed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e265100fd2dcd0ac0f6095ae19d2185",
+    "url": "/gradient_code/index.html"
+  },
+  {
+    "revision": "08a8412de03cea4f9018",
+    "url": "/gradient_code/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "2d7d3063aeec811209c4",
+    "url": "/gradient_code/static/css/main.2145d764.chunk.css"
+  },
+  {
+    "revision": "08a8412de03cea4f9018",
+    "url": "/gradient_code/static/js/2.071249d1.chunk.js"
+  },
+  {
+    "revision": "2d7d3063aeec811209c4",
+    "url": "/gradient_code/static/js/main.0d440cee.chunk.js"
+  },
+  {
+    "revision": "417f8161e39eedee5917",
+    "url": "/gradient_code/static/js/runtime~main.4e4289e5.js"
+  }
+]);
