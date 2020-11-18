ac1cc449bd0ab3066c904fe291af67929900e6d6
Shashank Sharma <shashank.ns67@gmail.com>
2020-11-18 21:37:27 +0530

    added hash routing

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL} hashType="slash">
         <App />
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
 
